a2f6512c1dd3d4bda417f656560ecea93a0bce63
stephp23 <stephp23@gmail.com>
2021-02-16 20:01:22 -0500

    changes to team and contact page titles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact">
-            <h1>Contact Us</h1>
+            <h1>Contact</h1>
             <p>Cookie apple pie donut pie dessert macaroon caramels. Sweet gingerbread halvah brownie jelly-o chocolate. Marzipan I love marzipan jelly-o tart chupa chups halvah candy canes chupa chups. Jujubes liquorice lemon drops jujubes. Jelly ice cream marshmallow cupcake jelly I love jelly marzipan I love. Gummi bears tiramisu danish pudding chocolate bar chocolate cupcake lemon drops candy canes.</p>
         </div>
     );
